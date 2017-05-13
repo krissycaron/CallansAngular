5f6cc41f806390a83a3d5d0bf9dae1e90cead27f
Kristin Caron <krissycaron@gmail.com>
2017-05-12 21:16:00 -0500

    cannot get the wierd image to show but button click works

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log("i'm attached");
+// console.log("i'm attached");
+
+var app = angular.module("CallanIsWeird", []);
+
+app.controller("imageShow", ($scope)=>{
+	$scope.image = ["http://yadbw.com/wp-content/uploads/2017/03/strange_people_1.jpg"]
+	
+	$scope.showListView = true;
+
+	$scope.showWierd= () =>{
+		$scope.showListView=false;
+	} 
+});
+
+
+
