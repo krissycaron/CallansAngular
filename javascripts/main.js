711dbad6ec2ceafdb16bd7b8b009567baa8e1dee
Kristin Caron <krissycaron@gmail.com>
2017-05-13 09:36:08 -0500

    background image show/hide

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,12 +3,13 @@
 var app = angular.module("CallanIsWeird", []);
 
 app.controller("imageShow", ($scope)=>{
-	$scope.image = ["http://yadbw.com/wp-content/uploads/2017/03/strange_people_1.jpg"]
+	$scope.image = "http://yadbw.com/wp-content/uploads/2017/03/strange_people_1.jpg"
 	
-	$scope.showListView = true;
+	$scope.showImageView = false;
 
 	$scope.showWierd= () =>{
-		$scope.showListView=false;
+		$scope.showImageView=!$scope.showImageView;
+		// console.log("hi im weird");
 	} 
 });
 
